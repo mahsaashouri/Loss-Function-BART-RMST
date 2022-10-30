364008c7845fe87b350cdff8d283b73d66841fd8
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2022-10-30 09:29:28 -0700

    Add mising term in tree probabilities

--- D_probabilities.R
+++ D_probabilities.R
@@ -7,25 +7,25 @@ DProb <- function(alpha, beta, d){
     prob <- 1-alpha
   else{
     if(d == 2){
-      m = r = h = 0; l = 2;
+      m = r = h = f = 0; l = 2;
     }else if(d == 3 | d == 4){
-      m = l = 1; r = 0; h = 2;
+      m = l = 1; r = f = 0; h = 2;
     }else if(d == 5){
-      m = 2; l = r = 0; h = 4;
+      m = 2; l = r = f = 0; h = 4;
     }else if(d == 6 | d == 7 | d == 9 | d == 10){
-      m = l = r = h = 1;
+      m = l = r = h = 1; f = 2;
     }else if(d == 8 | d == 11){
-      m = l = 1; r = 2; h = 0;
+      m = l = 1; r = 2; h = 0; f = 4;
     }else if(d == 12 | d == 13 | d == 14 | d == 15){
-      m = 2; l = 0; r = 1; h = 3;
+      m = 2; l = 0; r = 1; h = 3; f = 2;
     }else if(d == 16 | d == 17 | d == 18 | d == 19 | d == 20 | d == 21){
-      m = r = h = 2; l = 0;
+      m = r = h = 2; l = 0; f = 4;
     }else if(d == 22 | d == 23 | d == 24 | d == 25){
-      m = 2; l = 0; r = 3; h = 1;
+      m = 2; l = 0; r = 3; h = 1; f = 6;
     }else{
-      m = 2; l = 0; r = 4; h = 0;
+      m = 2; l = 0; r = 4; h = 0; f = 8;
     }
-    prob <- alpha*(alpha/(2^beta))^m*(1-(alpha/(2^beta)))^l*(alpha/(3^beta))^r*(1-(alpha/(3^beta)))^h
+    prob <- alpha*(alpha/(2^beta))^m*(1-(alpha/(2^beta)))^l*(alpha/(3^beta))^r*(1-(alpha/(3^beta)))^h*(1-(alpha/(4^beta)))^f
   }   
   return(prob)
 }
@@ -34,7 +34,7 @@ DProb <- function(alpha, beta, d){
 ## test the sum of probabilities
 SumprobD <- c()
 for(i in 1:26){
-  SumprobD[i] <- DProb(alpha = .95, beta = 1, i)
+  SumprobD[i] <- DProb(alpha = .95, beta = 2, i)
   
 }
 sum(SumprobD)
