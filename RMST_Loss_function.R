50a6904555f753abcd59d785f6031051f861228c
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2022-11-05 14:24:44 -0700

    Fix inputs in RMST loss function

--- RMST_Loss_function.R
+++ RMST_Loss_function.R
@@ -3,7 +3,7 @@
 ## U1, ..., Un: times, delta1, ..., deltan: event indicators, Yi = min(Ui, tau)
 ## G is the survival function of the censoring time distribution, i.e., G(t) = P {Ci > t}
 
-RMST_Loss <- function(U, tau, delta, Gvec, fitted_vector, J){
+RMST_Loss <- function(U, delta, Gvec, fitted_vector){
   #c <- c(); cvec <- c(); N <- c(); Nvec <- c();
   # for(j in 1:J){
   #   for(i in 1:n){
@@ -18,7 +18,7 @@ RMST_Loss <- function(U, tau, delta, Gvec, fitted_vector, J){
   #   g[j] <- Gvec[j-1] - Gvec[j]
   #   loss.c[j] <- (cvec[j]*log(g[j])) + ((Nvec[j]-cvec[j])*log(1-g[j]))
   # }
-  logY <- c(); loss.RMST <- c();
+  loss.RMST <- c();
   for(i in 1:n){
     loss.RMST[i] <- (delta[i]/Gvec(U[i]))*(log(U[i])-fitted_vector[i])^2 #+ sum(loss.c)
   }
