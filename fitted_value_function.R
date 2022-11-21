b80659bb4e378a422b8d619476fb53b7d780d71e
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2022-11-20 19:09:51 -0800

    Improved fittted value function for matrix of covariates

--- fitted_value_function.R
+++ fitted_value_function.R
@@ -1,7 +1,7 @@
 
 
-## Inputs: Predictors, Splitting values and variables, mu vectors , and tree structure (from 26 possible structures)
-## Output: Fitted mu value
+## Inputs: matrix of covariates, Splitting values and variables, mu vectors , and tree structure (from 26 possible structures)
+## Output: Fitted mu vector
 
 LeftChild <- function(x){
   y <- c(2, 4, 6, 8, 10, 12, 14)
@@ -13,40 +13,45 @@ RightChild <- function(x){
 }
 
 
-FittedValue <- function(xvec, splt.vals.raw, splt.vars.raw, muvec.raw, dvec){
+FittedValue <- function(xmat, splt.vals.raw, splt.vars.raw, muvec.raw, dvec){
   if(dvec[1] == 2){
     non_terminal <- FALSE
     mu.hat <- muvec.raw
   }
   else{
-    non_terminal <- TRUE
-    current.node <- 1
     ## update mu vector
     muvec <- replace(dvec, dvec!=2, NA)
     muvec[ which(!is.na(muvec))] <- muvec.raw
     ## update splitting variable vector
     splt.vars <- replace(dvec, dvec!=1, NA)
     splt.vars[ which(!is.na(splt.vars))] <- splt.vars.raw
-    current.var <- splt.vars[1]
-
+    ## update splitting value vector
     splt.vals <- replace(dvec, dvec!=1, NA)
     splt.vals[ which(!is.na(splt.vals))] <- splt.vals.raw
   }
-  while(non_terminal){
-    if(xvec[current.var] <= splt.vals[current.node]){
-      new_node <- LeftChild(current.node)
-      new.var <- splt.vars[new_node]
-    } else {
+  mu.hat <- c()
+  for(i in 1:nrow(xmat)){
+    xvec <- xmat[i,]
+    non_terminal <- TRUE
+    current.node <- 1
+    current.var <- splt.vars[1]
+    while(non_terminal){
+      if(xvec[current.var] <= splt.vals[current.node]){
+        new_node <- LeftChild(current.node)
+        new.var <- splt.vars[new_node]
+      } else {
         new_node <- RightChild(current.node)
         new.var <- splt.vars[new_node]
+      }
+      if(dvec[new_node] == 2){
+        non_terminal <- FALSE
+        mu.hat_ <- muvec[new_node]
+      } else {
+        current.var <- new.var
+        current.node <- new_node
+      }
     }
-    if(dvec[new_node] == 2){
-      non_terminal <- FALSE
-      mu.hat <- muvec[new_node]
-    } else {
-      current.var <- new.var
-      current.node <- new_node
-    }
+    mu.hat <- c(mu.hat, mu.hat_)
   }
   return(mu.hat)
 }
@@ -63,10 +68,5 @@ muvec.raw <- c(0.1, 0.03, 0.2, 0, 1, 4)
 splt.vars.raw <- c('x1', 'x3', 'x5','x4','x1')
 #splt.vars.raw <- c(1, 3, 5, 4, 1)
 dvec <- c(1, 1, 1, 2, 2, 1, 1, 0, 0, 0, 0, 2, 2, 2, 2)
-FittedValue(xvec1, splt.vals.raw, splt.vars.raw, muvec.raw, dvec)
-FittedValue(xvec2, splt.vals.raw, splt.vars.raw, muvec.raw, dvec)
-FittedValue(xvec3, splt.vals.raw, splt.vars.raw, muvec.raw, dvec)
-FittedValue(xvec4, splt.vals.raw, splt.vars.raw, muvec.raw, dvec)
-FittedValue(xvec5, splt.vals.raw, splt.vars.raw, muvec.raw, dvec)
-FittedValue(xvec6, splt.vals.raw, splt.vars.raw, muvec.raw, dvec)
-
+xmat <- rbind(xvec1, xvec2, xvec3, xvec4, xvec5, xvec6)
+FittedValue(xmat, splt.vals.raw, splt.vars.raw, muvec.raw, dvec)
