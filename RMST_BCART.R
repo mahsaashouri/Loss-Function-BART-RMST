b4ca7f09027ec9c5a2c46972acb3773595c6e344
Nicholas Henderson <nchender@Nicholass-iMac.attlocal.net>
2022-12-08 21:27:06 -0500

    simplified the calculation of the posterior mean sampling a bit

--- RMST_BCART.R
+++ RMST_BCART.R
@@ -18,7 +18,7 @@ RMST_BCART <- function(Y, delta, X, tree, ndraws, sigma.mu, muvec,sgrid, alpha, 
   n <- length(U)
   FittedValues <- matrix(NA, nrow=n, ncol=ndraws)
   old_tree <- tree
-  
+
   ## old_tree holds the splitting vars, splitting values, and dvec
   for(k in 1:ndraws) {
     # Step 1: Update Tree
@@ -31,7 +31,7 @@ RMST_BCART <- function(Y, delta, X, tree, ndraws, sigma.mu, muvec,sgrid, alpha, 
     } else if(move_type==3) {
       proposed_tree <- ChangeMove(old_tree, xmat)
     }
-    MH_ratio <- RMST_MHRatio(U = U, new_tree = proposed_tree, old_tree = old_tree, muvec = muvec, sigma.mu, 
+    MH_ratio <- RMST_MHRatio(U = U, new_tree = proposed_tree, old_tree = old_tree, muvec = muvec, sigma.mu,
                              Gvec, X = xmat, m = move_type, alpha, beta, ntree)
     if(is.na(MH_ratio) == TRUE){
       MH_ratio <- 1
@@ -42,22 +42,27 @@ RMST_BCART <- function(Y, delta, X, tree, ndraws, sigma.mu, muvec,sgrid, alpha, 
     } else {
       new_tree <- old_tree
     }
-    
+
     ## Step 2: Update the mu values -  sample from a Normal distribution
     ## number of terminal nodes in the new tree
     terminal_nodes <- which(new_tree$dvec==2)
-    
+
     ## get mean and sigma for updating mu values
     AT <- AMatrix(xmat, new_tree$splt.vals, new_tree$splt.vars, new_tree$dvec)
-    WTG <- t(AT)%*%solve(diag(Gvec))%*%AT
+    #WTG <- t(AT)%*%solve(diag(Gvec))%*%AT
+    WTGDiag <- c(crossprod(AT, 1/Gvec))
     VG <- U/Gvec
     Z <- c(crossprod(AT, VG))
-    
+
     ## update mu values
-    mu.mean <- (WTG+(sigma.mu^2*diag(1, length(terminal_nodes))))%*%matrix(Z, ncol=1)
-    mu.var <- solve(WTG+(sigma.mu^(-2)*diag(1,length(terminal_nodes))))
-    muvec <- c(mu.mean) + diag(sqrt(mu.var))*rnorm(length(terminal_nodes))
-      
+    #mu.mean <- (WTG+(sigma.mu^2*diag(1, length(terminal_nodes))))%*%matrix(Z, ncol=1)
+    #mu.var <- solve(WTG+(sigma.mu^(-2)*diag(1,length(terminal_nodes))))
+    #muvec <- c(mu.mean) + diag(sqrt(mu.var))*rnorm(length(terminal_nodes))
+    mu.mean <- Z/(WTGDiag + 1/(sigma.mu*sigma.mu))
+    mu.sd <- sqrt(1/(WTGDiag + 1/(sigma.mu*sigma.mu)))
+    muvec <- c(mu.mean) + mu.sd*rnorm(length(terminal_nodes))
+
+
     ## Record fitted values at each step
     FittedValues[,k] <- FittedValue(xmat, new_tree$splt.vals, new_tree$splt.vars, muvec, new_tree$dvec)
   }
