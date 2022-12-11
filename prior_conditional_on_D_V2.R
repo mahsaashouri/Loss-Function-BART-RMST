5d0a3e1c246e0564fb2d97a1b5284d79f168dee1
Nicholas Henderson <nchender@Nicholass-iMac.attlocal.net>
2022-12-11 15:26:02 -0500

    changed the ProbD function so that it can handle the case with 0 internal nodes and made a few other changes ... probably want to use this version in the future

---
+++ prior_conditional_on_D_V2.R
@@ -0,0 +1,38 @@
+ProbD <- function(U, X, splt.vals, splt.vars, muvec,
+                  alpha, beta, d, tau){
+  n <- length(U)
+  l <- length(splt.vars)
+  #PrD <- (DProb(alpha, beta, d))^(l)
+  PrD <- DProb(alpha, beta, d) ## don't need to raise to power l here
+  log_PrMu <- sum( dnorm(muvec, mean=0, sd=tau, log=TRUE) )
+
+  if(l==0) {
+     # return PrD*PrMu
+     ans <- exp(log_PrMu + log(PrD))
+  } else{
+     log_PrV <- -l*log( ncol(X) )
+     log_PrM <- 0
+     for(i in 1:l){
+       xm <- X[,splt.vars[i]]
+       #Pr[i] <- sum(which(xm == xm[i]))/n  ## Not sure why this was here?
+       splt_prob <- mean( which(xm == splt.vals[i]) )
+       log_PrM <- log(splt_prob) + log_PrM
+     }
+     ans <- exp(log_PrM + log_PrMu + log_PrV + log(PrD))
+     return(ans)
+  }
+}
+
+U <- c( 17, 14, 2, 0.4, 11)
+xmat <- matrix(round(rnorm(20,10),0), ncol = 4)
+colnames(xmat) <- c('x1', 'x2', 'x3', 'x4')
+splt.vals <- c('x1' = .05, 'x3' = .5, 'x2' = 1, 'x4' = .02)
+muvec <- c(0.1, 0.03, 0.2, 0, 1, 4)
+splt.vars <- c('x1', 'x3', 'x2','x4')
+dvec <- c(1, 1, 1, 2, 2, 1, 1, 0, 0, 0, 0, 2, 2, 2, 2)
+
+tst <- ProbD(U = U, X = xmat, splt.vals = splt.vals, splt.vars = splt.vars, muvec = muvec,
+      alpha = .95, beta = 2, d = 16, tau = 1)
+
+
+
