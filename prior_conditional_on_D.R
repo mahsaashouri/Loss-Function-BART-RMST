f083ff469bf5ab2b31c06e882daf2780b6ad45c9
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2022-12-15 18:16:49 -0800

    Fixed the example in prior-conditional-on-D

--- prior_conditional_on_D.R
+++ prior_conditional_on_D.R
@@ -8,13 +8,13 @@
 ProbD <- function(U, X, splt.vals, splt.vars, muvec, alpha, beta, d, tau){
   n <- length(U)
   l <- length(splt.vars)
-  PrD <- (DProb(alpha, beta, d))^(l)
-  PrV <- (1/ncol(X))^(l)
+  PrD <- (DProb(alpha, beta, d))#^(l)
+  PrV <- ncol(X)^(-l)
   PrMu <- prod((1/tau)*dnorm(muvec/tau))
   PrM <- 1; Pr <- c();
   for(i in 1:l){
     xm <- X[,splt.vars[i]]
-    Pr[i] <- sum(which(xm == xm[i]))/n
+    Pr[i] <- mean( which(xm == splt.vals[i]) )
     PrM <- Pr[i]*PrM
   }
   return(PrM*PrMu*PrV*PrD)
@@ -25,7 +25,7 @@ ProbD <- function(U, X, splt.vals, splt.vars, muvec, alpha, beta, d, tau){
 U <- c( 17, 14, 2, 0.4, 11)
 xmat <- matrix(round(rnorm(20,10),0), ncol = 4)
 colnames(xmat) <- c('x1', 'x2', 'x3', 'x4')
-splt.vals <- c('x1' = .05, 'x3' = .5, 'x2' = 1, 'x4' = .02)
+splt.vals <- c('x1' = xmat[1,1], 'x3' = xmat[2,3], 'x2' = xmat[5,2], 'x4' = xmat[4,4])
 muvec <- c(0.1, 0.03, 0.2, 0, 1, 4)
 splt.vars <- c('x1', 'x3', 'x2','x4')
 dvec <- c(1, 1, 1, 2, 2, 1, 1, 0, 0, 0, 0, 2, 2, 2, 2)
--- prior_conditional_on_D_V2.R
+++ prior_conditional_on_D_V2.R
@@ -2,19 +2,15 @@ ProbD <- function(U, X, splt.vals, splt.vars, muvec,
                   alpha, beta, d, tau){
   n <- length(U)
   l <- length(splt.vars)
-  #PrD <- (DProb(alpha, beta, d))^(l)
-  PrD <- DProb(alpha, beta, d) ## don't need to raise to power l here
+  PrD <- DProb(alpha, beta, d) 
   log_PrMu <- sum( dnorm(muvec, mean=0, sd=tau, log=TRUE) )
-
   if(l==0) {
-     # return PrD*PrMu
      ans <- exp(log_PrMu + log(PrD))
   } else{
      log_PrV <- -l*log( ncol(X) )
      log_PrM <- 0
      for(i in 1:l){
        xm <- X[,splt.vars[i]]
-       #Pr[i] <- sum(which(xm == xm[i]))/n  ## Not sure why this was here?
        splt_prob <- mean( which(xm == splt.vals[i]) )
        log_PrM <- log(splt_prob) + log_PrM
      }
@@ -26,7 +22,7 @@ ProbD <- function(U, X, splt.vals, splt.vars, muvec,
 U <- c( 17, 14, 2, 0.4, 11)
 xmat <- matrix(round(rnorm(20,10),0), ncol = 4)
 colnames(xmat) <- c('x1', 'x2', 'x3', 'x4')
-splt.vals <- c('x1' = .05, 'x3' = .5, 'x2' = 1, 'x4' = .02)
+splt.vals <- c('x1' = xmat[1,1], 'x3' = xmat[2,3], 'x2' = xmat[5,2], 'x4' = xmat[4,4])
 muvec <- c(0.1, 0.03, 0.2, 0, 1, 4)
 splt.vars <- c('x1', 'x3', 'x2','x4')
 dvec <- c(1, 1, 1, 2, 2, 1, 1, 0, 0, 0, 0, 2, 2, 2, 2)
