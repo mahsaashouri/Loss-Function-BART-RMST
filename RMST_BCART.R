e6400683818d9f0397823f7565d4166a3f85ea7a
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2022-12-20 08:48:06 -0800

    Moved proposedTree inside RMST-BCART

--- RMST_BCART.R
+++ RMST_BCART.R
@@ -1,4 +1,17 @@
 
+## function to return the proposed tree
+
+ProposedTree <- function(move_type, old_tree, xmat){
+  if(move_type==1) {
+    proposed_tree <- GrowMove(old_tree, xmat)
+  } else if(move_type==2) {
+    proposed_tree <- PruneMove(old_tree, xmat)
+  } else if(move_type==3) {
+    proposed_tree <- ChangeMove(old_tree, xmat)
+  }
+  return(proposed_tree)
+}
+
 RMST_BCART <- function(Y, delta, X, ntree, ndraws, sigma.mu, muvec,sgrid, alpha, beta, num.risk, num.events, kappa0) {
   ## Skeleton of function for computing
   ## Bayesian CART for the RMST loss function
