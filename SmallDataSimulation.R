6560d233e2ce0d4ddb81593ce390c89c92c6ed73
Nicholas Henderson <nchender@Nicholass-iMac.attlocal.net>
2022-12-27 15:15:46 -0500

    Added the full small, single-covariate simulation example

--- SmallDataSimulation.R
+++ SmallDataSimulation.R
@@ -1,16 +1,46 @@
 
 ## A way to generate a small dataset to test
 ##  the RMST_BCART function
+source("A_matrix.R")
+source("ChangeMove.R")
+source("D_probabilities.R")
+source("GPDraw.R")
+source("GrowMove.R")
+source("LogLik.R")
+source("PruneMove.R")
+source("PruneMove_V2.R")
+source("RMST_BCART.R")
+source("RMST_Loss.R")
+source("RMST_MHRatio.R")
+source("Recursive_A_matrix.R")
+source("Transition_Prob.R")
+source("fitted_value.R")
+source("prior_conditional_on_D.R")
+source("prior_conditional_on_D_V2.R")
+source("tree-configuration.R")
+
+
 n <- 100
 xx <- runif(n)
-TT <- rep(NA, n)
-TT[xx < 1/3] <- 500 + rnorm(sum(xx < 1/3), mean=0, sd=100)
-TT[xx >= 1/3 & xx < 2/3] <- 750 + rnorm(sum(xx >= 1/3 & xx < 2/3), mean=0, sd=100)
-TT[xx >= 2/3] <- 1000 + rnorm(sum(xx >= 2/3), mean=0, sd=100)
-CC <- rexp(n, rate=0.002)
+mean_fn <- rep(NA, n)
+mean_fn[xx < 1/3] <- 4
+mean_fn[xx >= 1/3 & xx < 2/3] <- 6
+mean_fn[xx > 2/3] <- 8
 
-Y <- pmin(TT, CC)
-Y <- pmin(Y, 1100)
-delta <- ifelse(TT <= CC, 1, 0)
+logT <- mean_fn + rnorm(n, mean=0, sd=2)
+logCC <- rexp(n, rate=0.2)
+
+Y <- pmin(logT, logCC)
+Y <- pmin(Y, 10)
+delta <- ifelse(logT <= logCC, 1, 0)
 X <- matrix(xx, nrow=n, ncol=1)
+colnames(X) <- 'x'
+sgrid <- seq(0, 10, by=.1)
+
+
+test_run <- RMST_BCART(Y, delta, X, ntree=1, ndraws=5, sigma.mu=1.2, muvec=muvec,
+                       sgrid=sgrid, alpha=0.95, beta=2, num.risk=0, num.events=0, kappa0=1)
+
+
+
 
