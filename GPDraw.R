3d1dd855a2a1ff1f649d7b198d5fc2b307dd3eca
Nicholas Henderson <nchender@Nicholass-iMac.attlocal.net>
2023-01-08 12:05:12 -0500

    fixed issue with negative numbers as input

--- GPDraw.R
+++ GPDraw.R
@@ -1,5 +1,5 @@
-GPDraw <- function(U, sgrid, num.risk, num.events, kappa0) {
-  # U - the vector of observed-follow up times
+GPDraw <- function(eU, sgrid, num.risk, num.events, kappa0) {
+  # eU - the vector of observed-follow up times (exp of input to RMST_BCART)
   # sgrid - grid of J+1 points of the form (s_{j-1}, s_{j}] for
   #          the grouped likelihood function
   # num.risk - vector of length J; number at risk at time s_{j-1}
@@ -14,9 +14,7 @@ GPDraw <- function(U, sgrid, num.risk, num.events, kappa0) {
   lambda.vec <- rgamma(J, shape=alpha.diff + num.events, rate=kappa0 + num.risk - num.events)
   Lambda.vec <- c(0, cumsum(lambda.vec))
   LamFn <- approxfun(sgrid, Lambda.vec)
-  print(LamFn(2))
-  GPweights <- LamFn(U)
-  print(GPweights)
+  GPweights <- LamFn(eU)
 
   return(GPweights)
 }
@@ -41,7 +39,8 @@ ComputeSurvStatistics <- function(sgrid, times, status) {
 #lam.draw <- GPDraw(U=cancer$time, sgrid=sgrid, num.risk=SS$n.risk,
 #                   num.events=SS$n.event, kappa0=1)
 
-
-
-
-
+#SS <- ComputeSurvStatistics(sgrid=sgrid, times=exp(Y), status=1 - delta)
+#
+#lam.draw <- GPDraw(U=U, sgrid=sgrid, num.risk=SS$n.risk,
+#                   num.events=SS$n.event, kappa0=1)
+#Gvec <- exp(-lam.draw)
