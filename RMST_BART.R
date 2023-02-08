3bd3756344fd940ee859901f36fb2ae129015c3e
Nicholas Henderson <nchender@Nicholass-iMac.attlocal.net>
2023-02-08 15:44:52 -0500

    just changed X.test argument so the default is NULL

--- RMST_BART.R
+++ RMST_BART.R
@@ -12,11 +12,11 @@ ProposedTree <- function(move_type, old_tree, xmat){
   return(proposed_tree)
 }
 
-## old.tree is a list of initial trees 
+## old.tree is a list of initial trees
 
-RMST_BART <- function(Y, delta, X, old.tree, ndraws, sigma.mu,
+RMST_BART <- function(Y, delta, X, old.tree, X.test=NULL, ndraws=100, sigma.mu=1,
                       alpha=0.95, beta=2, kappa0=1, sgrid=NULL, tau=NULL, burnIn=100) {
-  
+
   ## organize data
   if(ncol(X) > 1) {
     xmat <- X[delta==1,]
@@ -27,7 +27,7 @@ RMST_BART <- function(Y, delta, X, old.tree, ndraws, sigma.mu,
   if(is.null(tau)) {
     tau <- max(Y[delta==1])
   }
-  
+
   #U <- pmin(Y[delta==1], tau)
   U <- pmin(Y, tau)
   ## get Gvec
@@ -38,7 +38,7 @@ RMST_BART <- function(Y, delta, X, old.tree, ndraws, sigma.mu,
   lam.draw <- GPDraw(eU=exp(U), sgrid=sgrid, num.risk=SS$n.risk,
                      num.events=SS$n.event, kappa0=1)
   Gvec <- exp(-lam.draw)
-  
+
   n <- length(U)
   ## initialize fitted values
   #FittedValues <- matrix(NA, nrow = n,  ncol = length(old.tree))
@@ -46,16 +46,20 @@ RMST_BART <- function(Y, delta, X, old.tree, ndraws, sigma.mu,
   #  FittedValues[,i] <- FittedValue(xmat, old.tree[[i]]$splt.vals, old.tree[[i]]$splt.vars, muvec, old.tree[[i]]$dvec)
   #}
   FittedValues <- matrix(0, nrow = n,  ncol = length(old.tree))
-  FittedValues.test <- matrix(0, nrow = nrow(X.test),  ncol = length(old.tree))
+  if(!is.null(X.test)) {
+     FittedValues.test <- matrix(0, nrow = nrow(X.test),  ncol = length(old.tree))
+  } else {
+     FittedValues.test <- NULL
+  }
   NNodes <- loglikvals <- matrix(NA, nrow = (ndraws+burnIn), ncol = length(old.tree))
-  
+
   Fitted.Values <- list()
   for(j in 1:(ndraws+burnIn)){
     for(k in 1:length(old.tree)) {
-      
+
       # update tree
       ## sample one of three move types
-      
+
       move_type <- sample(1:3, size=1)
       proposed_tree <- ProposedTree(move_type,  old.tree[[k]], xmat)
       if ("character" %in% class(proposed_tree)){
@@ -64,52 +68,56 @@ RMST_BART <- function(Y, delta, X, old.tree, ndraws, sigma.mu,
           proposed_tree <- ProposedTree(move_type,  old.tree[[k]], xmat)
         }
       }
-      
+
       # update Rs (U.res)
-      U.res <- U - (rowSums(FittedValues) - FittedValues[,k]) 
-      
+      U.res <- U - (rowSums(FittedValues) - FittedValues[,k])
+
       ## compute the ratio
       MH_ratio <- RMST_MHRatio(U = U.res, new_tree = proposed_tree, old_tree = old.tree[[k]], sigma.mu,
                                Gvec, X = X, m = move_type, alpha, beta, length(old.tree), tau = tau)
-      
+
       u <- runif(1)
       if(u <= MH_ratio) {
         new_tree <- proposed_tree
       } else {
         new_tree <- old.tree[[k]]
       }
-      
+
       ## update the mu values -  sample from a normal distribution
       ## number of terminal nodes in the new tree
       terminal_nodes <- which(new_tree$dvec==2)
-      
+
       ## get mean and sigma for updating mu values
       AT <- AMatrix(xmat, new_tree$splt.vals, new_tree$splt.vars, new_tree$dvec)
       WTGDiag <- c(crossprod(AT, 1/Gvec[delta==1]))
       VG <- U[delta==1]/Gvec[delta==1]
       Z <- c(crossprod(AT, VG))
-      
+
       ## update mu values
-      
+
       mu.mean <- Z/(WTGDiag + 1/(sigma.mu*sigma.mu))
       mu.sd <- sqrt(1/(WTGDiag + 1/(sigma.mu*sigma.mu)))
       muvec <- c(mu.mean) + mu.sd*rnorm(length(terminal_nodes))
-      
+
       ## Record fitted values at each step
       FittedValues[,k] <- FittedValue(X, new_tree$splt.vals, new_tree$splt.vars, muvec, new_tree$dvec)
-      
+
       ## record number of internal nodes and loglikelihood values
       NNodes[j,k] <- sum(new_tree$dvec==1)
       loglikvals[j,k] <- LogLik(tree=new_tree, X=xmat, U=U.res[delta==1], Gvec=Gvec[delta==1], sigma.mu=sigma.mu)
-      if(j==(ndraws+burnIn)){
-        FittedValues.test[,k] <- FittedValue(X.test, new_tree$splt.vals, new_tree$splt.vars, muvec, new_tree$dvec)
+
+      if(!is.null(X.test)) {
+         if(j==(ndraws+burnIn)){
+            FittedValues.test[,k] <- FittedValue(X.test, new_tree$splt.vals,
+                                                 new_tree$splt.vars, muvec, new_tree$dvec)
+         }
       }
       ## replace old_tree with new_tree
       old.tree[[k]] <- new_tree
     }
     Fitted.Values[[length(Fitted.Values)+1]] <- FittedValues
   }
-  ans <- list(fitted.values=Fitted.Values[(burnIn+1):(ndraws+burnIn)], nnodes=tail(NNodes, ndraws), 
+  ans <- list(fitted.values=Fitted.Values[(burnIn+1):(ndraws+burnIn)], nnodes=tail(NNodes, ndraws),
               logliks=tail(loglikvals, ndraws), fitted.values.test=FittedValues.test)
   return(ans)
 }
