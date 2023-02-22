d46292fa87bf3e0d4edae53bc7437cb2c8845131
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2023-02-22 14:20:27 -0500

    Added test set results in friedman simulation study

--- friedman_simulation_study.R
+++ friedman_simulation_study.R
@@ -32,6 +32,7 @@ f.test <- function(x) {10*sin(pi*x[ , 1]*x[ , 2]) + 20*(x[ , 3]-.5)^2+10*x[ , 4]
 
 sigma = 1.0
 n = 250 # 250 or 2000 # number of training observation
+n.test = 2000 # 2000 or 4000 # number of test observation
 k = 10 # 10 or 100 # total number of predictors
 ndraws <- 500
 sgrid <- seq(0, 10, by=.1)
@@ -67,6 +68,7 @@ CoxExpectedSurv <- function(X, beta_val, H0fn, tau) {
 cens_prop <- rep(NA, nreps)
 rmse_bcart <- rmse_coxph <- rmse_rcoxph <- rmse_sboost <- rmse_aft <- rmst_aft_bart<- rmse_aft_null <- rep(NA, nreps)
 for(j in 1:nreps) {
+    ## training set
     X.train <- matrix(runif(n*k), n, k)
     colnames(X.train) <- paste0('X', 1:k)
     ET.train <- f.test(X.train)
@@ -77,48 +79,61 @@ for(j in 1:nreps) {
     C.train <- runif(n, min=.5, max=3) ## min = 0.5 or 2
     Y.train <- pmin(T.train, C.train)
     delta.train <- ifelse(T.train <= C.train, 1, 0) ## mean delta train 50-60 % or 80-90 %
-    mean(delta.train)
-    bcart_mod <- RMST_BCART(Y.train, delta.train, X.train, ndraws=500, tau=500, sigma.mu=1.2)
-    bcart_fitted <- pmin(rowMeans(bcart_mod$fitted.values), log(tau))
+    
+    ## test set
+    X.test <- matrix(runif(n.test*k), n.test, k)
+    colnames(X.test) <- paste0('X', 1:k)
+    ET.test <- f.test(X.test)
+    mu.test <- digamma(gam_alph) - log(ET.test)
+    T.test <- rgamma(n.test, shape=gam_alph, rate=ET.test)
+    C.test <- runif(n.test, min=.5, max=3) ## min = 0.5 or 2
+    Y.test <- pmin(T.test, C.test)
+    delta.test <- ifelse(T.test <= C.test, 1, 0)
+    
+    ## RMST-BCART
+    bcart_mod <- RMST_BCART(Y.train, delta.train, X.train, X.test, ndraws=500, tau=500, sigma.mu=1.2)
+    bcart_fitted <- pmin(rowMeans(bcart_mod$fitted.values.test), log(tau))
     
     ## Coxph
     COXPH.mod <- coxph(Surv(Y.train, delta.train) ~ X.train)
     coxhaz <- basehaz(COXPH.mod)
     H0fn <- approxfun(c(0, coxhaz$time), c(0, coxhaz$hazard),
                       yright=max(coxhaz$hazard))
-    COXPH <- CoxExpectedSurv(X=X.train, beta_val=COXPH.mod$coefficients,
+    COXPH <- CoxExpectedSurv(X=X.test, beta_val=COXPH.mod$coefficients,
                     H0fn=H0fn, tau=1)
     COXPH_fitted <- pmin(COXPH, log(tau))
     
     ## regularized coxph model
     RCOXPH <-  glmnet(X.train, Surv(Y.train, delta.train), family = "cox", lambda = 1, alpha = 1)
-    RCOXPH_fitted <- pmin(c(predict(RCOXPH, X.train, type = 'response')), log(tau))
+    RCOXPH_fitted <- pmin(c(predict(RCOXPH, X.test, type = 'response')), log(tau))
     
     ## survival boosting
     SBOOST <- glmboost(Surv(Y.train, delta.train)~X.train, family = Gehan(), control = boost_control(mstop = 300))
-    SBOOST_fitted <- pmin(c(predict(SBOOST)), log(tau))
-    sqrt(mean((SBOOST_fitted - mu.train)*(SBOOST_fitted - mu.train)))
-
+    ## we have warnings in predict function: 'newdata' had 2000 rows but variables found have 250 rows  
+    SBOOST_fitted <- pmin(c(predict(SBOOST, newdata = data.frame(X.test))), log(tau))
+    
     ## AFT
     AFT <- survreg(Surv(Y.train, delta.train) ~ X.train)
-    AFT_fitted <- pmin(AFT$linear.predictors, log(tau))
+    ## we have warnings in predict function: 'newdata' had 2000 rows but variables found have 250 rows  
+    AFT_fitted <- pmin(predict(AFT, newdata = data.frame(X.test), type = 'response'), log(tau))
+    #AFT_fitted <- pmin(AFT$linear.predictors, log(tau))
     
     ## AFT BART
-    AFT_BART <- abart(X.train, Y.train, delta.train)
-    AFT_BART_fitted <-  pmin(AFT_BART$yhat.train.mean, log(tau))
+    AFT_BART <- abart(X.train, Y.train, x.test = X.test, delta.train)
+    AFT_BART_fitted <-  pmin(AFT_BART$yhat.test.mean, log(tau))
 
     ## AFT null
     AFT_null <- survreg(Surv(Y.train, delta.train) ~ 1)
-    AFT_null_fitted <- pmin(AFT_null$linear.predictors, log(tau))
+    AFT_null_fitted <- pmin(predict(AFT_null, newdata = data.frame(X.test), type = 'response'), log(tau))
 
-    rmse_bcart[j] <- sqrt(mean((bcart_fitted - mu.train)*(bcart_fitted - mu.train)))
-    rmse_coxph[j] <- sqrt(mean((COXPH_fitted - mu.train)*(COXPH_fitted - mu.train)))
-    rmse_rcoxph[j] <- sqrt(mean((RCOXPH_fitted - mu.train)*(RCOXPH_fitted - mu.train)))
-    rmse_sboost[j] <- sqrt(mean((SBOOST_fitted - mu.train)*(SBOOST_fitted - mu.train)))
-    rmse_aft[j] <- sqrt(mean((AFT_fitted - mu.train)*(AFT_fitted - mu.train)))
-    rmst_aft_bart[j] <- sqrt(mean((AFT_BART_fitted - mu.train)*(AFT_BART_fitted - mu.train)))
-    rmse_aft_null[j] <- sqrt(mean((AFT_null_fitted - mu.train)*(AFT_null_fitted - mu.train)))
-    cens_prop[j] <- mean(delta.train) # also record censoring proportion
+    rmse_bcart[j] <- sqrt(mean((bcart_fitted - mu.test)*(bcart_fitted - mu.test)))
+    rmse_coxph[j] <- sqrt(mean((COXPH_fitted - mu.test)*(COXPH_fitted - mu.test)))
+    rmse_rcoxph[j] <- sqrt(mean((RCOXPH_fitted - mu.test)*(RCOXPH_fitted - mu.test)))
+    rmse_sboost[j] <- sqrt(mean((SBOOST_fitted - mu.test)*(SBOOST_fitted - mu.test)))
+    rmse_aft[j] <- sqrt(mean((AFT_fitted - mu.test)*(AFT_fitted - mu.test)))
+    rmst_aft_bart[j] <- sqrt(mean((AFT_BART_fitted - mu.test)*(AFT_BART_fitted - mu.test)))
+    rmse_aft_null[j] <- sqrt(mean((AFT_null_fitted - mu.test)*(AFT_null_fitted - mu.test)))
+    cens_prop[j] <- mean(delta.test) # also record censoring proportion
 }
 
 
