89579eb01ae167e444da2a8a9272240cc22a0d96
Nicholas Henderson <nchender@Nicholass-iMac.attlocal.net>
2023-02-23 14:02:38 -0500

    added results record

--- friedman_simulation_study.R
+++ friedman_simulation_study.R
@@ -66,7 +66,7 @@ CoxExpectedSurv <- function(X, beta_val, H0fn, tau) {
 
 
 cens_prop <- rep(NA, nreps)
-rmse_bcart <- rmse_coxph <- rmse_rcoxph <- rmse_sboost <- rmse_aft <- rmst_aft_bart<- rmse_aft_null <- rep(NA, nreps)
+rmse_bcart <- rmse_coxph <- rmse_rcoxph <- rmse_sboost <- rmse_aft <- rmse_aft_bart<- rmse_aft_null <- rep(NA, nreps)
 for(j in 1:nreps) {
     ## training set
     X.train <- matrix(runif(n*k), n, k)
@@ -79,7 +79,7 @@ for(j in 1:nreps) {
     C.train <- runif(n, min=.5, max=3) ## min = 0.5 or 2
     Y.train <- pmin(T.train, C.train)
     delta.train <- ifelse(T.train <= C.train, 1, 0) ## mean delta train 50-60 % or 80-90 %
-    
+
     ## test set
     X.test <- matrix(runif(n.test*k), n.test, k)
     colnames(X.test) <- paste0('X', 1:k)
@@ -89,11 +89,11 @@ for(j in 1:nreps) {
     C.test <- runif(n.test, min=.5, max=3) ## min = 0.5 or 2
     Y.test <- pmin(T.test, C.test)
     delta.test <- ifelse(T.test <= C.test, 1, 0)
-    
+
     ## RMST-BCART
     bcart_mod <- RMST_BCART(Y.train, delta.train, X.train, X.test, ndraws=500, tau=500, sigma.mu=1.2)
     bcart_fitted <- pmin(rowMeans(bcart_mod$fitted.values.test), log(tau))
-    
+
     ## Coxph
     COXPH.mod <- coxph(Surv(Y.train, delta.train) ~ X.train)
     coxhaz <- basehaz(COXPH.mod)
@@ -102,22 +102,22 @@ for(j in 1:nreps) {
     COXPH <- CoxExpectedSurv(X=X.test, beta_val=COXPH.mod$coefficients,
                     H0fn=H0fn, tau=1)
     COXPH_fitted <- pmin(COXPH, log(tau))
-    
+
     ## regularized coxph model
     RCOXPH <-  glmnet(X.train, Surv(Y.train, delta.train), family = "cox", lambda = 1, alpha = 1)
     RCOXPH_fitted <- pmin(c(predict(RCOXPH, X.test, type = 'response')), log(tau))
-    
+
     ## survival boosting
     SBOOST <- glmboost(Surv(Y.train, delta.train)~X.train, family = Gehan(), control = boost_control(mstop = 300))
-    ## we have warnings in predict function: 'newdata' had 2000 rows but variables found have 250 rows  
+    ## we have warnings in predict function: 'newdata' had 2000 rows but variables found have 250 rows
     SBOOST_fitted <- pmin(c(predict(SBOOST, newdata = data.frame(X.test))), log(tau))
-    
+
     ## AFT
     AFT <- survreg(Surv(Y.train, delta.train) ~ X.train)
-    ## we have warnings in predict function: 'newdata' had 2000 rows but variables found have 250 rows  
+    ## we have warnings in predict function: 'newdata' had 2000 rows but variables found have 250 rows
     AFT_fitted <- pmin(predict(AFT, newdata = data.frame(X.test), type = 'response'), log(tau))
     #AFT_fitted <- pmin(AFT$linear.predictors, log(tau))
-    
+
     ## AFT BART
     AFT_BART <- abart(X.train, Y.train, x.test = X.test, delta.train)
     AFT_BART_fitted <-  pmin(AFT_BART$yhat.test.mean, log(tau))
@@ -131,11 +131,25 @@ for(j in 1:nreps) {
     rmse_rcoxph[j] <- sqrt(mean((RCOXPH_fitted - mu.test)*(RCOXPH_fitted - mu.test)))
     rmse_sboost[j] <- sqrt(mean((SBOOST_fitted - mu.test)*(SBOOST_fitted - mu.test)))
     rmse_aft[j] <- sqrt(mean((AFT_fitted - mu.test)*(AFT_fitted - mu.test)))
-    rmst_aft_bart[j] <- sqrt(mean((AFT_BART_fitted - mu.test)*(AFT_BART_fitted - mu.test)))
+    rmse_aft_bart[j] <- sqrt(mean((AFT_BART_fitted - mu.test)*(AFT_BART_fitted - mu.test)))
     rmse_aft_null[j] <- sqrt(mean((AFT_null_fitted - mu.test)*(AFT_null_fitted - mu.test)))
     cens_prop[j] <- mean(delta.test) # also record censoring proportion
 }
 
 
+nmethods <- 7
+Results <- matrix(NA, nrow=nmethods, ncol=2)
+rownames(Results) <- c("AFT Null", "CoxPH", "Cox glmnet", "AFT linear",
+                       "Boosting", "BCART", "AFT BART")
+colnames(Results) <- c("Mean RMSE", "Median RMSE")
 
+Results[1,1:2] <- c(mean(rmse_aft_null), median(rmse_aft_null))
+Results[2,1:2] <- c(mean(rmse_coxph), mean(rmse_coxph))
+Results[3,1:2] <- c(mean(rmse_rcoxph), median(rmse_rcoxph))
+Results[4,1:2] <- c(mean(rmse_aft), median(rmse_aft))
+Results[5,1:2] <- c(mean(rmse_sboost), median(rmse_sboost))
+Results[6,1:2] <- c(mean(rmse_bcart), median(rmse_bcart))
+Results[7,1:2] <- c(mean(rmse_aft_bart), median(rmse_aft_bart))
+
+round(Results, 4)
 
