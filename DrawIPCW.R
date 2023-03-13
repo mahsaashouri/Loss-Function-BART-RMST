310b6033b07f7a6e184a8648b6c79db77f1ec793
Nicholas Henderson <nchender@0587664673.vpn.umich.net>
2023-03-12 23:45:00 -0400

    changed approach to purely rejection sampling

--- DrawIPCW.R
+++ DrawIPCW.R
@@ -1,41 +1,8 @@
-LogSumExp <- function(lseq) {
-    ## This function computes
-    ##.   sum(exp(lseq))
-
-    cstar <- max(lseq)
-    ans <- exp(cstar)*sum(exp(lseq - cstar))
-    return(ans)
-}
-
 
 LambdaCDF <- function(t, nevent, nrisk, c0, delta_alpha) {
 
-  even_seq <- seq(0, nevent, by=2)
-  prob_even <- (nrisk - nevent)/(nrisk - nevent + c0 + even_seq)
-
-  cstar_even <- max(lchoose(nevent, even_seq) + c0*delta_alpha*log(prob_even))
-  even_seq <- lchoose(nevent, even_seq) + c0*delta_alpha*log(prob_even) - cstar_even
-  sum_even <- exp(cstar_even)*sum( exp(even_seq)   )
-
-  if(nevent > 0) {
-    odd_seq <- seq(1, nevent, by=2)
-    prob_odd <- (nrisk - nevent)/(nrisk - nevent + c0 + odd_seq)
-
-    cstar_odd <- max(lchoose(nevent, odd_seq) + c0*delta_alpha*log(prob_odd))
-    odd_seq <- lchoose(nevent, odd_seq) + c0*delta_alpha*log(prob_odd) - cstar_odd
-    sum_odd <- exp(cstar_odd)*sum( exp(odd_seq)   )
-  }
-
-
-  if(nevent > 0) {
-    *(sum_even - sum_odd)
-
-  } else {
-    *sum_even
-  }
   tmp <- (((-1)^(0:nevent))*choose(nevent, 0:nevent))/((nrisk - nevent + c0 + 0:nevent)^(c0*delta_alpha))
   ww <- tmp/sum(tmp)
-  print(tmp)
   ff <- pgamma(t, shape=c0*delta_alpha, rate=nrisk - nevent + c0 + 0:nevent)
   FF <- sum(ww*pgamma(t, shape=c0*delta_alpha, rate=nrisk - nevent + c0 + 0:nevent))
   return(FF)
@@ -45,7 +12,6 @@ InvLambdaCDF <- function(u, nevent, nrisk, c0, delta_alpha) {
   ff <- function(tt, u, nevent, nrisk, c0, delta_alpha) {
     return( LambdaCDF(tt, nevent, nrisk, c0, delta_alpha) - u)
   }
-  #ulimit <- abs(qgamma(u, shape=c0*delta_alpha, rate=nrisk - nevent + c0 + 0:nevent))
   ## need a better way to figure out ulimit
   ulimit <- 100
   print(c(nrisk, nevent))
@@ -56,27 +22,55 @@ InvLambdaCDF <- function(u, nevent, nrisk, c0, delta_alpha) {
                  u = u, nevent=nevent, nrisk=nrisk, c0=c0, delta_alpha=delta_alpha)$root
   return(ans)
 }
-DrawLambdas <- function(nevents, nrisks, c0, delta_alpha) {
+
+DrawLambdas <- function(nevents, nrisks, kappa0, delta_alpha) {
   nbins <- length(nevents)
   lambda_draw <- rep(NA, nbins)
-  for(j in 1:nbins) {
-    u <- runif(1)
-    lambda_draw[j] <- InvLambdaCDF(u, nevents[j], nrisks[j], c0, delta_alpha)
+  eta <- kappa0*delta_alpha
+  bbeta <- nrisks - nevents + kappa0
+  for(k in 1:nbins) {
+    done <- FALSE
+    while(!done) {
+      theta <- rgamma(1, shape=eta + nevents[k], rate=bbeta[k])
+      u <- runif(1)
+      thresh <- log1p(-exp(-theta)) - log(theta)
+      if(log(u) < nevents[k]*thresh) {
+         lambda_draw[k] <- theta
+         done <- TRUE
+      }
+    }
   }
   return(lambda_draw)
 }
 
-DrawIPCW <- function(U, delta, Utau, sgrid, c0, delta_alpha) {
+DrawIPCW <- function(U, delta, Utau, sgrid, kappa0, delta_alpha) {
   J <- length(sgrid)
   E <- R <- rep(NA, J-1)
   for(j in 2:J) {
     E[j-1] <- sum((1 - delta)*(U > sgrid[j-1])*(U <= sgrid[j]))
     R[j-1] <- sum(U > sgrid[j-1])
   }
-  lambda.draw <- DrawLambdas(nevents=E, nrisks=R, c0=c0, delta_alpha=delta_alpha)
+  lambda.draw <- DrawLambdas(nevents=E, nrisks=R, kappa0=kappa0, delta_alpha=delta_alpha)
   CumHazFn <- approxfun(sgrid, cumsum(c(0, lambda.draw)))
   weight_ans <- exp(CumHazFn(Utau))
   return(weight_ans)
 }
+
+
+## Testing it out
+#n <- 500
+#TT <- rexp(n)
+#CC <- rexp(n, rate=2.5)
+#sgrid <- seq(0, 10, length.out=500)
+
+#U <- pmin(TT, CC)
+#delta <- ifelse(TT <= CC, 1, 0)
+
+#tt <- DrawIPCW(U=U, delta=delta, Utau=U, sgrid=sgrid,
+#               kappa0=1, delta_alpha=1)
+
+#xx <- seq(0, 3, length.out=1000)
+#plot(U[order(tt)], 1/tt[order(tt)])
+#lines(xx, exp(-2.5*xx), lwd=3, col="red")
 
 
