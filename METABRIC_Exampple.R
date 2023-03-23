486a559c3a221b32b99f04bd14d5e7cc0c29da65
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2023-03-23 18:03:00 -0400

    Started working on METABRIC dataset

---
+++ METABRIC_Exampple.R
@@ -0,0 +1,15 @@
+
+library(tidyverse)
+METABRIC <- read.csv('METABRIC_RNA_Mutation.csv', header = TRUE)[,c(1:31)]
+
+METABRIC <- METABRIC %>% 
+  select(-c('patient_id', 'cancer_type', 'cancer_type_detailed'))
+## missing values in all columns are around 600 rows
+METABRIC <- na.omit(METABRIC)
+
+DATA <- METABRIC[ , !(names(METABRIC) %in% c('overall_survival'))]
+DATA <- model.matrix(overall_survival_months~.-1, data = DATA)
+Y <- METABRIC$overall_survival_months
+delta <- METABRIC$overall_survival
+sgrid <- seq(0, 4000, by=1)
+
