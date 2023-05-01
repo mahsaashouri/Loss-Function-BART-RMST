b2b06aa73ce2d691477f3c909a9ba4b1f6016ed8
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2023-05-01 10:26:28 -0400

    Replaced integrate with discrete CoxExpectedSurv function

--- multinormAR1_simulation_study.R
+++ multinormAR1_simulation_study.R
@@ -28,8 +28,8 @@ set.seed(123)
 
 
 sigma <- 1.0
-n <- 2000 # 250 or 2000 # number of training observation
-n.test <- 4000 # 2000 or 4000 # number of test observation
+n <- 250 # 250 or 2000 # number of training observation
+n.test <- 2000 # 2000 or 4000 # number of test observation
 num_covar <- 10 # 10 or 100 # total number of predictors
 ndraws <- 500
 sgrid <- seq(0, 10, by=.1)
@@ -39,9 +39,9 @@ Rho <- 0.5
 #Warning message:
 # In regularize.values(x, y, ties, missing(ties), na.rm = na.rm) :
 #  collapsing to unique 'x' values
-tau <- 500
+tau <- 50
 gam_alpha <- 20
-nreps <- 3 # number of simulation replications
+nreps <- 1 # number of simulation replications
 
 ## function to simulate linear model AR1
 sim.reg <- function(nobs, coef, mu, sd, Rho){ 
@@ -56,24 +56,40 @@ sim.reg <- function(nobs, coef, mu, sd, Rho){
 }
 
 ## function we need for cox model
-CoxExpectedSurv <- function(X, beta_val, H0fn, tau) {
+#CoxExpectedSurv <- function(X, beta_val, H0fn, tau) {
+  ## This function computes E( min(T_i, tau) |x_i) for
+  ## a cox ph model
+ # mu.x <- colMeans(X)
+  #integrand <- function(time, xi, beta_val) {
+   # nu <- sum(xi*beta_val)
+    #ans <- exp(-H0fn(time)*exp(nu))
+    #return(ans)
+  #}
+  #nn <- nrow(X)
+  #fitted_vals <- rep(NA, nn)
+  #for(k in 1:nn) {
+   # II <- integrate(integrand, lower=0, upper=tau, xi=X[k,] - mu.x,
+    #                beta_val=beta_val, subdivisions=5000L)
+    #fitted_vals[k] <- II$value
+  #}
+  #return(fitted_vals)
+#}
+CoxExpectedSurv <- function(X, beta_val, time, H0.vals, tau) {
   ## This function computes E( min(T_i, tau) |x_i) for
   ## a cox ph model
   mu.x <- colMeans(X)
-  integrand <- function(time, xi, beta_val) {
-    nu <- sum(xi*beta_val)
-    ans <- exp(-H0fn(time)*exp(nu))
-    return(ans)
-  }
+  tpoints <- c(time[time < tau], tau)
+  Hpoints <- H0.vals[time < tau]
   nn <- nrow(X)
+  
   fitted_vals <- rep(NA, nn)
   for(k in 1:nn) {
-    II <- integrate(integrand, lower=0, upper=tau, xi=X[k,] - mu.x,
-                    beta_val=beta_val, subdivisions=50000L)
-    fitted_vals[k] <- II$value
+    nu <- sum((X[k,] - mu.x)*beta_val)
+    fitted_vals[k] <- sum( exp(-Hpoints*exp(nu)*diff(tpoints)) )
   }
   return(fitted_vals)
-}
+}  
+
 
 
 cens_prop <- rep(NA, nreps)
@@ -99,8 +115,8 @@ for(j in 1:nreps) {
   Y.test <- pmin(T.test, C.test)
   delta.test <- ifelse(T.test <= C.test, 1, 0) ## mean delta train 50-60 % or 80-90 %
   #mu.test <- digamma(gam_alpha) - log(DataSim.test$Y)
-  mu.test <- (DataSim.test$Y/gam_alph)*pgamma(tau, shape = gam_alph+1, rate = DataSim.test$Y) + 
-    tau*pgamma(tau, shape = gam_alph, rate = DataSim.test$Y, lower.tail = FALSE)
+  mu.test <- (DataSim.test$Y/gam_alpha)*pgamma(tau, shape = gam_alpha+1, rate = DataSim.test$Y) + 
+    tau*pgamma(tau, shape = gam_alpha, rate = DataSim.test$Y, lower.tail = FALSE)
   
   ### 1. AFT linear model
   AFT <- survreg(Surv(Y.train, delta.train) ~ X.train)
@@ -157,9 +173,9 @@ for(j in 1:nreps) {
                     yright=max(coxhaz$cumhaz))
   ## for some datasets returns this error: Error in integrate(integrand, lower = 0, upper = tau, xi = X[k, ] - mu.x, :
   #maximum number of subdivisions reached
-  COXPH_fitted <- CoxExpectedSurv(X=X.test, beta_val=COXPH.mod$coefficients,
-                                  H0fn=H0fn, tau=tau)
-  
+  COXPH_fitted <- CoxExpectedSurv(X=X.test, beta_val=COXPH.mod$coefficients, time = DataSim.test$Y,
+                                  H0.vals=H0fn, tau=tau)
+
   ### 6. Cox-PH model with lasso penalty
   rcox_tmp <- cv.glmnet(x=X.train, y=Surv(Y.train, delta.train), family = "cox",
                         type.measure = "deviance")
@@ -169,8 +185,8 @@ for(j in 1:nreps) {
   RH0fn <- approxfun(c(0, Rcoxhaz$time), c(0, Rcoxhaz$cumhaz),
                      yright=max(Rcoxhaz$cumhaz))
   lasso_betahat <- as.numeric(coef(RCOXPH))
-  RCOXPH_fitted <- CoxExpectedSurv(X=X.test, beta_val=lasso_betahat,
-                                   H0fn=RH0fn, tau=tau)
+  RCOXPH_fitted <- CoxExpectedSurv(X=X.test, beta_val=lasso_betahat, time, 
+                                   H0.vals=RH0fn, tau=tau)
   
   ## 7. RMST BCART
   bcart_mod <- RMST_BCART(Y.train, delta.train, X.train, X.test,
