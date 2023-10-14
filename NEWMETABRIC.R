60b4c67d733db801447e588453cdee4431a26cb8
Nicholas Henderson <nchender@Nicholass-iMac.attlocal.net>
2023-10-14 09:01:23 -0400

    new analysis with cross-validation performed for scale hyperparameter

--- NEWMETABRIC.R
+++ NEWMETABRIC.R
@@ -16,8 +16,8 @@ METABRIC <- read.csv('METABRIC_RNA_Mutation.csv', header = TRUE)[,c(1:41)]
 ######################
 
 ## drop unwanted columns
-METABRIC <- METABRIC %>% 
-  select(-c('patient_id', 'cancer_type', 'cancer_type_detailed', 'her2_status_measured_by_snp6', 'er_status', 
+METABRIC <- METABRIC %>%
+  select(-c('patient_id', 'cancer_type', 'cancer_type_detailed', 'her2_status_measured_by_snp6', 'er_status',
             'death_from_cancer', 'X3.gene_classifier_subtype', 'oncotree_code', 'cellularity'))
 ## check number of missing in each column of the dataset
 colSums(is.na(METABRIC))
@@ -70,6 +70,22 @@ METABRIC$cohort <- as.factor(METABRIC$cohort)
 table(METABRIC$overall_survival)
 METABRIC$overall_survival <- ifelse(METABRIC$overall_survival == 1, 0, ifelse(METABRIC$overall_survival == 0, 1,
                                                                               METABRIC$overall_survival))
+
+names(METABRIC)[which(names(METABRIC)=="pam50_._claudin.low_subtype")] <- "molecular_subtype"
+names(METABRIC)[which(names(METABRIC)=="tumor_subtype")] <- "histol_subtype"
+names(METABRIC)[which(names(METABRIC)=="pr_status")] <- "PR_"
+names(METABRIC)[which(names(METABRIC)=="brca1")] <- "BRCA1"
+names(METABRIC)[which(names(METABRIC)=="brca2")] <- "BRCA1"
+names(METABRIC)[which(names(METABRIC)=="palb2")] <- "PALB2"
+names(METABRIC)[which(names(METABRIC)=="pten")] <- "PTEN"
+names(METABRIC)[which(names(METABRIC)=="tp53")] <- "TP53"
+names(METABRIC)[which(names(METABRIC)=="atm")] <- "ATM"
+names(METABRIC)[which(names(METABRIC)=="cdh1")] <- "CDH1"
+names(METABRIC)[which(names(METABRIC)=="chek2")] <- "CHEK2"
+names(METABRIC)[which(names(METABRIC)=="nbn")] <- "NBN"
+names(METABRIC)[which(names(METABRIC)=="nf1")] <- "NF1"
+
+
 ############################
 ## running different methods
 ############################
@@ -78,116 +94,130 @@ METABRIC$overall_survival <- ifelse(METABRIC$overall_survival == 1, 0, ifelse(ME
 set.seed(123)
 ## 400 rows as test set (~20%)
 DATA <- METABRIC[ , !(names(METABRIC) %in% c('overall_survival'))]
-DATA <- model.matrix(overall_survival_months~.-1, data = DATA)
+X.train <- model.matrix(overall_survival_months~.-1, data = DATA)
 
 
 # Define the number of iterations and the proportion of data to be used for training
-n_iterations <- 1
-train_prop <- 0.7
 tau <- 300
 sgrid <- seq(0, tau, by=100)
-gam_alph <- 20
-sigma <- 1.0
 ndraws <- 2000
-burnIn <- 100
+burnIn <- 500
 
-bcart_fitted <- bart_fitted_ind <- bart_fitted_dep <- AFT_BART_fitted <- list()
-# Loop through the iterations
-for (j in 1:n_iterations) {
-  
-  # Randomly select index numbers for the training and test sets
-  train_idx <- sample(1:nrow(DATA), round(train_prop * nrow(DATA)), replace = FALSE)
-  test_idx <- setdiff(1:nrow(DATA), train_idx)
-  
-  # Create training and test sets using the selected index numbers
-  train.set <- DATA[train_idx, ]
-  test.set <- DATA[test_idx, ]
-  
-  Y <- METABRIC[train_idx,]$overall_survival_months
-  delta <- METABRIC[train_idx,]$overall_survival
-  
-  Y.test <- METABRIC[test_idx,]$overall_survival_months
-  rate.test <- 1 + Y.test
-  mu.test <- Y.test*pgamma(tau, shape = rate.test+1, rate = rate.test) +
-    tau*pgamma(tau, shape = rate.test, rate = rate.test, lower.tail = FALSE)
-  
-  delta.test <- METABRIC[test_idx,]$overall_survival
-  
-  ##  RMST BART
-  delta_alpha <- 1
-  kappa0 <- 1
-  U_tau <- pmin(Y[delta==1], tau)
-  Gmat <- matrix(1, nrow=ndraws + burnIn + 1, ncol=length(U_tau))
-  for(k in 1:(ndraws + burnIn + 1)) {
-    Gmat[k,] <- DrawIPCW(U=Y, delta=delta, Utau=U_tau, sgrid=sgrid,
-                         kappa0=kappa0, delta_alpha=delta_alpha)
-  }
-  Gmat <- 1/sqrt(Gmat)
-  
-  bart_mod <- RMST_BART(Y, delta, train.set, Gweights=Gmat,
-                        x.test=test.set, tau=tau, k = 2,
-                        ndpost=ndraws, nskip=burnIn, ntree = 200)
-  bart_mod$yhat.train <- pmax(bart_mod$yhat.train, 0)
-  bart_mod$yhat.test <- pmax(bart_mod$yhat.test, 0)
-  bart_mod$yhat.train.mean <- colMeans(bart_mod$yhat.train)
-  bart_mod$yhat.test.mean <- colMeans(bart_mod$yhat.test)
-  
-  bart_fitted_ind[[j]] <- bart_mod
-  BART_CI <- t(apply(bart_mod$yhat.test, 1, function(x) quantile(x, probs=c(0.025, 0.975))))
-  
-  
-  cens_bart <- AFTrees(x.train=train.set, y.train=Y, status=1-delta,
-                       ndpost=ndraws + burnIn, verbose=FALSE)
-  Mucens_draws <- cens_bart$m.train
-  GmatDep <- matrix(1, nrow=ndraws + burnIn + 1, ncol=length(U_tau))
-  for(k in 1:(ndraws + burnIn)) {
-    for(h in 1:length(U_tau)) {
-      log.time.points <- log(U_tau[h])
-      AA <- (log.time.points - cens_bart$locations[k,] - Mucens_draws[k,h])/cens_bart$sigma[k]
-      Cprob <- sum(pnorm(AA, lower.tail=FALSE)*cens_bart$mix.prop[k,])
-      GmatDep[k,h] <- 1/Cprob
-    }
-  }
-  GmatDep <- 1/sqrt(GmatDep)
-  bart_dep_mod <- RMST_BART(Y, delta, train.set, Gweights=GmatDep,
-                            x.test=test.set, tau=tau, k = 2.0,
-                            ndpost=ndraws, nskip=burnIn, ntree = 200)
-  bart_dep_mod$yhat.train <- pmax(bart_dep_mod$yhat.train, 0)
-  bart_dep_mod$yhat.test <- pmax(bart_dep_mod$yhat.test, 0)
-  bart_dep_mod$yhat.train.mean <- colMeans(bart_dep_mod$yhat.train)
-  bart_dep_mod$yhat.test.mean <- colMeans(bart_dep_mod$yhat.test)
-  
-  bart_fitted_dep[[j]] <- bart_dep_mod
-  BART_dep_CI <- t(apply(bart_dep_mod$yhat.test, 1, function(x) quantile(x, probs=c(0.025, 0.975))))
-  
-  ## BCART
-  bcart_mod <- RMST_BART(Y, delta, train.set, Gweights=Gmat,
-                         x.test=test.set, tau=tau, k = 2, ntree=1L,
-                         ndpost=ndraws, nskip=burnIn)
-  
-  bcart_fitted[[j]] <- bcart_mod
-  
-  
-  #### 3. AFT_BART model
-  AFT_BART <- abart(train.set, Y, delta, x.test=test.set)
-  ndraw_abart <- nrow(AFT_BART$yhat.test)
-  AFT_fit_reps <- matrix(NA, nrow=ndraw_abart, ncol=nrow(test.set))
-  for(k in 1:ndraw_abart) {
-    aft_bart_mu <- AFT_BART$yhat.test[k,]
-    aft_bart_sig <- AFT_BART$sigma[k]
-    aft_bart_sigsq <- aft_bart_sig*aft_bart_sig
-    gt_prob <- pnorm((log(tau) - aft_bart_mu)/aft_bart_sig, lower.tail=FALSE)
-    lt_prob <- pnorm((log(tau) - aft_bart_sigsq - aft_bart_mu)/aft_bart_sig)
-    
-    AFT_fit_reps[k,] <- exp(aft_bart_sigsq/2 + aft_bart_mu)*lt_prob + tau*gt_prob
-  }
-  #AFT_BART_fitted[[i]] <- colMeans(AFT_fit_reps)
-  AFT_BART_fitted[[j]] <- AFT_fit_reps
+
+# Create training and test sets using the selected index numbers
+#train_idx <- sample(1:nrow(DATA), round(train_prop * nrow(DATA)), replace = FALSE)
+#test_idx <- setdiff(1:nrow(DATA), train_idx)
+#X.train <- DATA[train_idx, ]
+#X.test <- DATA[test_idx, ]
+
+Y <- METABRIC$overall_survival_months
+delta <- METABRIC$overall_survival
+
+###########################################
+## Draw Censoring weights for both
+## the independent and dependent cases
+###########################################
+delta_alpha <- 1
+kappa0 <- 1
+U_tau <- pmin(Y[delta==1], tau)
+Gmat <- matrix(1, nrow=ndraws + burnIn + 1, ncol=length(U_tau))
+for(k in 1:(ndraws + burnIn + 1)) {
+  Gmat[k,] <- DrawIPCW(U=Y, delta=delta, Utau=U_tau, sgrid=sgrid,
+                       kappa0=kappa0, delta_alpha=delta_alpha)
 }
+Gmat_orig <- 1/sqrt(Gmat)
 
+cens_bart <- AFTrees(x.train=X.train, y.train=Y, status=1-delta,
+                     ndpost=ndraws + burnIn, verbose=FALSE)
+Mucens_draws <- cens_bart$m.train
+GmatDep <- matrix(1, nrow=ndraws + burnIn + 1, ncol=length(U_tau))
+for(k in 1:(ndraws + burnIn)) {
+  for(h in 1:length(U_tau)) {
+    log.time.points <- log(U_tau[h])
+    AA <- (log.time.points - cens_bart$locations[k,] - Mucens_draws[k,h])/cens_bart$sigma[k]
+    Cprob <- sum(pnorm(AA, lower.tail=FALSE)*cens_bart$mix.prop[k,])
+    GmatDep[k,h] <- 1/Cprob
+  }
+}
+GmatDeporig <- 1/sqrt(GmatDep)
+
+#########################################
+## Get initial estimate of eta_hat
+AFT_try <- survreg(Surv(exp(Y), delta) ~ X.train)
+eta_hat <- AFT_try$scale*AFT_try$scale
+############################################
+
+########################################################
+## Now, do 5-fold cross-validation to get the best
+## value of eta_hat
+nfolds <- 5
+eta_hat_vals <- c(0.1*eta_hat, 0.5*eta_hat, 0.75*eta_hat, eta_hat)
+CVscore <- matrix(NA, nrow=nfolds, ncol=length(eta_hat_vals))
+X.train.obs <- X.train[delta==1,]
+Y.train.obs <- Y[delta==1]
+ntrain <- nrow(X.train.obs)
+fold_memb <- sample(1:nfolds, size=ntrain, replace=TRUE)
+CVscore <- CVscoreDep <- matrix(NA, nrow=nfolds, ncol=length(eta_hat_vals))
+cens_dist <- survfit(Surv(Y, 1-delta) ~ 1)
+GKM <- stepfun(c(0, cens_dist$time), c(1, cens_dist$surv, min(cens_dist$surv)))
+for(u in 1:length(eta_hat_vals)) {
+  Gmat <- sqrt(2*eta_hat_vals[u])*Gmat_orig
+  GmatDep <- sqrt(2*eta_hat_vals[u])*GmatDeporig
+  for(k in 1:nfolds) {
+    Y.train_tmp <- Y.train.obs[fold_memb!=k]
+    delta.train_tmp <- rep(1, sum(fold_memb!=k))
+    X.train_tmp <- X.train.obs[fold_memb!=k,]
+
+    Y.test_tmp <- Y.train.obs[fold_memb==k]
+    delta.test_tmp <- rep(1, sum(fold_memb==k))
+    X.test_tmp <- X.train.obs[fold_memb==k,]
+
+    Gmat_train_tmp <- Gmat[,fold_memb!=k]
+    Gmat_test_tmp <- Gmat[,fold_memb==k]
+
+    GmatDep_train_tmp <- GmatDep[,fold_memb!=k]
+    GmatDep_test_tmp <- GmatDep[,fold_memb==k]
+
+    ww <- 1/GKM(Y.test_tmp)
+
+    bartmod_tmp <- RMST_BART(Y.train_tmp, delta.train_tmp, X.train_tmp, Gweights=Gmat_train_tmp,
+                             x.test=X.test_tmp, tau=tau, k = 2, ndpost=ndraws, nskip=burnIn)
+
+    bartmod_dep_tmp <- RMST_BART(Y.train_tmp, delta.train_tmp, X.train_tmp, Gweights=GmatDep_train_tmp,
+                                 x.test=X.test_tmp, tau=tau, k = 2,
+                                 ndpost=ndraws, nskip=burnIn)
+
+    yhat <- bartmod_tmp$yhat.test.mean
+    yhat_dep <- bartmod_dep_tmp$yhat.test.mean
+
+    CVscore[k, u] <- mean(ww*((Y.test_tmp - yhat)*(Y.test_tmp - yhat)))
+    CVscoreDep[k,u] <- mean(ww*((Y.test_tmp - yhat_dep)*(Y.test_tmp - yhat_dep)))
+  }
+}
+CVfinal <- colMeans(CVscore)
+CVfinalDep <- colMeans(CVscoreDep)
+eta_hat_star <- eta_hat_vals[which.min(CVfinal)]
+eta_hat_star_dep <- eta_hat_vals[which.min(CVfinalDep)]
+#######################
+
+############################
+## Now using best CV parameters with RMST-BART
+#############################
+Gmat <- sqrt(2*eta_hat_star)*Gmat_orig
+bart_mod <- RMST_BART(Y, delta, x.train=X.train, Gweights=Gmat, tau=tau, k = 2,
+                      ndpost=ndraws, nskip=burnIn, ntree=200)
+bart_fitted <- bart_mod$yhat.train.mean
+BART_CI <- t(apply(bart_mod$yhat.train, 1, function(x) quantile(x, probs=c(0.025, 0.975))))
+
+GmatDep <- sqrt(2*eta_hat_star_dep)*GmatDeporig
+bart_dep_mod <- RMST_BART(Y, delta, x.train=X.train, Gweights=GmatDep, tau=tau, k = 2.0,
+                          ndpost=ndraws, nskip=burnIn, ntree = 200)
+
+bart_dep_fitted <- bart_dep_mod$yhat.train.mean
+BART_dep_CI <- t(apply(bart_dep_mod$yhat.train, 1, function(x) quantile(x, probs=c(0.025, 0.975))))
 
 ## plotting the first 10 repeated variables in one iteration - BART
-VarImp <- tail(sort(colSums(bart_fitted_ind[[1]]$varcount)),10)
+VarImp <- sort(colSums(bart_mod$varcount), decreasing=TRUE)[1:15]
 
 library(ggplot2)
 # Create a data frame with the numbers and names
@@ -201,7 +231,7 @@ ggplot(VarImpDataF, aes(x = seq_along(numbers), y = numbers)) +
   #geom_text(aes(label = names), hjust = -.5, vjust = -.9, size = 6, color = "blue") +
   #geom_text_repel(aes(label = names), size = 6, color = "blue") +
   geom_label_repel(aes(label = names), size = 6, color = "blue") +
-  labs(x = "Variable", y = "Count") +
+  labs(x = "Variable", y = "Mean number of times used") +
   theme_light() +
   theme(
     axis.title.x = element_text(size = 15),
@@ -211,27 +241,16 @@ ggplot(VarImpDataF, aes(x = seq_along(numbers), y = numbers)) +
     legend.title=element_text(size=15),
     legend.text=element_text(size=15))
 
-## Average RMST
 
-bart.r <- bart_fitted_ind[[1]]$yhat.test.mean
-sqrt(mean((bart.r - mu.test)*(bart.r - mu.test)))
-bcart.r <- bcart_fitted[[1]]$yhat.test.mean
-sqrt(mean((bcart.r - mu.test)*(bcart.r - mu.test)))
-AFT.r <- colMeans(AFT_fit_reps)
-sqrt(mean((AFT.r - mu.test)*(AFT.r - mu.test)))
+## Confidence interval for each patient - plot
+means <- bart_mod$yhat.train.mean
+df_summary <- data.frame(row = 1:ncol(bart_mod$yhat.train), mean = means)
 
-
-## Confidence interval for each case - plot
-means <- bart_fitted_ind[[1]]$yhat.test.mean
-ses <- matrixStats::colSds(bart_fitted_ind[[1]]$yhat.test, na.rm=TRUE)
-df_summary <- data.frame(row = 1:ncol(bart_fitted_ind[[1]]$yhat.test), mean = means, se = ses)
-
-df_summary$max <- df_summary$mean + 1.96 * df_summary$se
-df_summary$min <- df_summary$mean - 1.96 * df_summary$se
-df_summary <- subset(df_summary, select = -c(se))
+df_summary$max <- apply(bart_mod$yhat.train, 2, function(x) quantile(x, probs=0.975))
+df_summary$min <- apply(bart_mod$yhat.train, 2, function(x) quantile(x, probs=0.025))
 ## plot sample of lines
-sample_idx <- sample(1:nrow(df_summary), round(0.01 * nrow(df_summary)), replace = FALSE)
-sample_df_summary <- df_summary[sample_idx,]
+#sample_idx <- sample(1:nrow(df_summary), round(0.01 * nrow(df_summary)), replace = FALSE)
+#sample_df_summary <- df_summary[sample_idx,]
 #sample_long <- reshape2::melt(sample_df_summary, id.vars = "row")
 
 # Sort the rows of df_summary by the mean column
@@ -239,103 +258,106 @@ df_summary_sorted <- df_summary[order(df_summary$mean),]
 
 # create the plot
 plot(1, type='n', xlim=c(min(df_summary_sorted$min), max(df_summary_sorted$max)),
-     ylim=c(0.5, nrow(df_summary_sorted)+0.5), ylab='Case number', xlab='Test set credible intervals')
-
+     ylim=c(0.5, nrow(df_summary_sorted)+0.5), ylab='Case number', xlab="Months ",
+     las=1)
 # loop through the rows and draw a line for each
 for (i in 1:nrow(df_summary)) {
   segments(df_summary_sorted$min[i], i, df_summary_sorted$max[i], i)
   points(df_summary_sorted$mean[i], i, pch=19, col='blue')
 }
 
-## Partial Dependence plots
 
-Col_ParDep <- c('atm', 'palb2', 'pten','nottingham_prognostic_index')
+##########################################
+## Partial Dependence plots
+###########################################
+
+Col_ParDep <- c('nottingham_prognostic_index', 'tumor_size', 'CHEK2', 'age_at_diagnosis')
 Y <- METABRIC$overall_survival_months
 delta <- METABRIC$overall_survival
 
-##  RMST BART
-U_tau <- pmin(Y[delta==1], tau)
-Gmat <- matrix(1, nrow=ndraws + burnIn + 1, ncol=length(U_tau))
-for(k in 1:(ndraws + burnIn + 1)) {
-  Gmat[k,] <- DrawIPCW(U=Y, delta=delta, Utau=U_tau, sgrid=sgrid,
-                       kappa0=kappa0, delta_alpha=delta_alpha)
-}
-Gmat <- 1/sqrt(Gmat)
-
-DATA <- METABRIC[ , !(names(METABRIC) %in% c('overall_survival'))]
-DATA <- model.matrix(overall_survival_months~.-1, data = DATA)
-ff <- matrix(NA, nrow = 100, ncol = 3)
+## Setup Gmatrix and PD grid points before starting the loop:
+Gmat <- sqrt(2*eta_hat_star)*Gmat_orig
+ngrid <- 25
+GridEnd <- rbind(c(2, 6), c(0, 60), c(-2, 2), c(40, 85))
+ff <- matrix(NA, nrow = ngrid, ncol = 3)
 partial_results <- list()
 for(i in 1:length(Col_ParDep)){
-  pp <- seq(min(METABRIC[,Col_ParDep[i]]),max(METABRIC[,Col_ParDep[i]]), length.out = 100)
-  for(k in 1:100){
-    
-    Xtmp <- DATA
+  #pp <- seq(min(METABRIC[,Col_ParDep[i]]),max(METABRIC[,Col_ParDep[i]]), length.out = ngrid)
+  pp <- seq(GridEnd[i,1], GridEnd[i,2], length.out=ngrid)
+  for(k in 1:ngrid){
+
+    Xtmp <- X.train
     Xtmp[,Col_ParDep[i]] <- rep(pp[k], nrow(DATA))
-    Y <- METABRIC$overall_survival_months
-    delta <- METABRIC$overall_survival
-    bart_mod <- RMST_BART(Y, delta, x.train=DATA, Gweights=Gmat, x.test=Xtmp, tau=tau, k = 2,
-                          ndpost=ndraws, nskip=burnIn)
-    ff[k,1] <- mean(colMeans(bart_mod$yhat.test))
+    ## change name of bart_mod here
+    bart_mod_tmps <- RMST_BART(Y, delta, x.train=X.train, Gweights=Gmat,
+                               x.test=Xtmp, tau=tau, k = 2,
+                               ndpost=ndraws, nskip=burnIn)
+    ff[k,1] <- mean(bart_mod_tmps$yhat.test.mean)
     ff[k,2] <- pp[k]
     ff[k,3] <- Col_ParDep[i]
+    print(c(i, k))
   }
   partial_results[[length(partial_results)+1]] <- ff
 }
 
-partial_results_all <- do.call("rbind", partial_results)
-colnames(partial_results_all) <- c('MeanPrediction', 'Value', 'index')
+partial_results_all <- as.data.frame(do.call("rbind", partial_results))
+names(partial_results_all) <- c('MeanPrediction', 'Value', 'index')
+partial_results_all$xlabels <- rep("", nrow(partial_results_all))
+partial_results_all$xlabels[partial_results_all$index =="age_at_diagnosis"] <- "Age at Diagnosis"
+partial_results_all$xlabels[partial_results_all$index =="nottingham_prognostic_index"] <- "Nottingham Prognostic Index"
+partial_results_all$xlabels[partial_results_all$index =="tumor_size"] <- "Tumor Size"
+partial_results_all$xlabels[partial_results_all$index =="CHEK2"] <- "CHEK2: mRNA expression level"
+
+
 
 library(ggplot2)
 library(patchwork)
 
 # Create a list to store the individual plots
 plots <- list()
-
 # Iterate over each category and create a plot
 for (category in unique(partial_results_all[,3])) {
-  subset_data <- as.data.frame(subset(partial_results_all, partial_results_all[,3] == category))
+  subset_data <- subset(partial_results_all, partial_results_all[,3] == category)
   plot <- ggplot(subset_data, aes(x = as.numeric(Value), y = as.numeric(MeanPrediction), group = index)) +
     geom_line() +
-    xlab(subset_data$index)+
+    xlab(subset_data$xlabels[1])+
     theme_light() +
     theme(axis.title = element_text(size = 22),  # Adjust the size of the axis titles
           axis.text = element_text(size = 20))
-  
+
   # Set y-axis label for the leftmost plots
   if (category %in% unique(partial_results_all[,3])[c(1,3)]) {
-    plot <- plot + ylab("Mean Prediction") #+ theme(axis.title.y = element_blank())
+    plot <- plot + ylab("Predicted RMST")
   } else {
     plot <- plot + theme(axis.title.y = element_blank(),
                          axis.text.y = element_blank())
     # Remove x-axis labels for the two plots on the right
-    #plot <- plot + scale_x_continuous(labels = NULL)
   }
-  
   plots[[category]] <- plot
 }
 
+## Need to change x-axis labels for these plots.
+## CHEK2: mRNA expression level
 # Combine the individual plots using patchwork
 combined_plot <- wrap_plots(plots, ncol = 2)
 
 # Display the combined plot
+# Make sure the y-axis limits are visible in each panel.
 combined_plot
 
-
 ## Posterior mean comparison
-
-PostMean_ind <- c(bart_fitted_ind[[1]]$yhat.train.mean, bart_fitted_ind[[1]]$yhat.test.mean)
-PostMean_dep <-  c(bart_fitted_dep[[1]]$yhat.train.mean, bart_fitted_dep[[1]]$yhat.test.mean)
+PostMean_ind <- bart_mod$yhat.train.mean
+PostMean_dep <-  bart_dep_mod$yhat.train.mean
 PostMean_data <- data.frame('Independent' = PostMean_ind, 'Dependent' = PostMean_dep)
 #write.csv(PostMean_data, 'PostMean_data_METABRIC.train.csv')
 
 # Plot the data
-
+## Might be useful to also include y=x line here.
+## Also, maybe make x-axis limits and y-axis limits the same.
 ggplot(PostMean_data, aes(x = Independent, y = Dependent)) +
   geom_point() +
-  labs(x = "Independent", y = "Dependent", title = "") +
+  labs(x = "Independent Censoring", y = "Dependent Censoring",
+       title = "Posterior Means for Different Censoring Models") +
   theme_light() +
   theme(axis.title = element_text(size = 22),  # Adjust the size of the axis titles
         axis.text = element_text(size = 20))   # Adjust the size of the axis labels
-
-
